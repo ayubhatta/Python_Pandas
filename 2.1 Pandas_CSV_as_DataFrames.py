33cc186b9d686da5dcef3ad13b269eb3e9dcdaed
Ayub Bhatta <118649309+ayubhatta@users.noreply.github.com>
2023-12-16 19:12:33 +0545

    Update 2.1 Pandas_CSV_as_DataFrames.py

--- 2.1 Pandas_CSV_as_DataFrames.py
+++ 2.1 Pandas_CSV_as_DataFrames.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\bhatt\Downloads\Salary_Data.csv')
+df = pd.read_csv(r'C:\Users\bhatt\Downloads\Salary_Data.csv')     # reading the csv file in my pc
 print(df)
 print("\n")
 
